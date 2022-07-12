45f4d923a1547311f6769ee394e0e02bb9628e8b
Atalia Lourenco <thaliacardoso55@gmail.com>
2022-07-12 14:27:47 +0100

    first commit

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+function hover(element) {
+  element.setAttribute("src", "/img/do-utilizador.png");
+}
+
+function unhover(element) {
+  element.setAttribute("src", "../img/user (2).png");
+}
+
+function hover1(element) {
+  element.setAttribute("src", "../img/decolar.png");
+}
+
+function unhover2(element) {
+  element.setAttribute("src", "../img/aviao.png");
+}
